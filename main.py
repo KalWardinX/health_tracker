b51f969a6a331cb3b181ec81e27ef8b7f6403798
Akash Rawat <akashrawat44444@gmail.com>
2024-11-06 01:42:58 +0530

    added form to fill meal data

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, flash, redirect, url_for
+from flask import Flask, render_template, flash, redirect, url_for, request
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, FloatField, IntegerField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, FloatField, IntegerField, SelectMultipleField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -8,6 +8,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Column, Integer, String, Float
 import json, string
+import time
 
 app = Flask(__name__)
 
@@ -19,7 +20,7 @@ from wtforms.validators import DataRequired
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 
-jsonfile = open('file.json', 'r')
+jsonfile = open('static/file.json', 'r')
 data = json.load(jsonfile)
 values = data['Nutrition Values']
 foods = []
@@ -28,11 +29,40 @@ for value in values:
     food = value["Food"]
     foods.append(food)
 
+from wtforms import SelectMultipleField, widgets
+from markupsafe import Markup
+ 
+ 
+class BootstrapListWidget(widgets.ListWidget):
+ 
+    def __call__(self, field, **kwargs):
+        kwargs.setdefault("id", field.id)
+        html = [f"<{self.html_tag} {widgets.html_params(**kwargs)}>"]
+        for subfield in field:
+            if self.prefix_label:
+                html.append(f"<li class='list-group-item'>{subfield.label} {subfield(class_='form-check-input ms-1')}</li>")
+            else:
+                html.append(f"<li class='list-group-item'>{subfield(class_='form-check-input me-1')} {subfield.label}</li>")
+        html.append("</%s>" % self.html_tag)
+        return Markup("".join(html))
+ 
+ 
+class MultiCheckboxField(SelectMultipleField):
+    widget = widgets.ListWidget(prefix_label=False)
+    option_widget = widgets.CheckboxInput()
+
+# class SearchForm(FlaskForm):
+#     __items = []
+#     i = 0
+#     for food_dict in data['Nutrition Values']:
+#         __items.append((i,food_dict['Food']))
+#         i+=1
+#     items = MultiCheckboxField('Food Items', choices=__items, option_widget=None)
+#     submit = SubmitField('Submit')
+
 class SearchForm(FlaskForm):
-    search = StringField('Search', validators=[DataRequired()])
-    submit = SubmitField('Search')
-
-
+    food_items = TextAreaField("food-items", validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 app.config['SECRET_KEY'] = '1856a607f1d8fc35957b1566f7e9030a'
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///tech-stack.db"
@@ -66,6 +96,9 @@ class Nutrition(Base):
     Sat_fat = Column(Float)
     carbs = Column(Float)
     fiber = Column(Float)
+
+    def __str__(self):
+        return f'{self.id}, Day={self.day}, Wt={self.weight}, Ht={self.height}, Protein={self.protein}, Cal={self.calories}, fat={self.fat}, SAt={self.Sat_fat}, carb={self.carbs}, fib={self.fiber})'
 
 class Exercise(Base):
     __tablename__ = 'exercise'
@@ -102,16 +135,56 @@ class LoginForm(FlaskForm):
 
 @app.route("/home", methods=["GET", "POST"])
 def home():
-
+    __food_items = []
+    _food_dict = {}
+    i = 0
+    for food_dict in data['Nutrition Values']:
+        __food_items.append((i,food_dict['Food']))
+        _food_dict[food_dict['Food']] = i
+        i+=1
     form = SearchForm()
-    selected_values = []
-    
-    if request.method == 'POST' and form.validate_on_submit():
-        search_term = form.search.data
-        filtered_values = [food for food in foods if search_term in food]
-        return render_template('home.html', title="Home", form=form, results=filtered_values, selected_values=selected_values)
+    selected_items = request.form.getlist('items')
+    # print(selected_items)
+    # print('hi')
+    if request.method == 'POST':
+        # push to database
+        print('hi')
+        flash(f'Selcected Food Items: {", ".join(form.food_items.data)}', 'success')
+        print(form.food_items.data)
+        protein = 0
+        calories = 0
+        fat = 0
+        Sat_fat = 0
+        carbs = 0
+        fiber = 0
+        for i in form.food_items.data.split('; '):
+            _i = i.split('(x')[0]
+            print(type(_i))
+            print(_i)
+            food = data['Nutrition Values'][_food_dict[_i]]
+            print(food)
+            protein += float(food['Protein'].replace(',','')) if food['Protein']!='t' else 0
+            fat += float(food['Fat'].replace(',','')) if food['Fat']!='t' else 0
+            Sat_fat += float(food['Sat.Fat'].replace(',','')) if food['Sat.Fat']!='t' else 0
+            carbs += float(food['Carbs'].replace(',','')) if food['Carbs']!='t' else 0
+            fiber += float(food['Fiber'].replace(',','')) if food['Fiber']!='t' else 0
+            calories += float(food['Calories'].replace(',','')) if food['Calories']!='t' else 0
+            
+        nutrition_obj = Nutrition(
+            day = time.localtime().tm_yday,
+            protein = protein,
+            calories = calories,
+            fat = fat,
+            Sat_fat = Sat_fat,
+            carbs = carbs,
+            fiber = fiber
+        )
+        print(nutrition_obj)
+        session.add(nutrition_obj)
+        session.commit()
+        return redirect(url_for('home'))
 
-    return render_template("home.html", title="Home", form=form, results=[], selected_values=selected_values)
+    return render_template("home2.html", title="Home", form=form, data=__food_items)
 
 @app.route("/")
 @app.route("/login", methods=["GET", "POST"])
